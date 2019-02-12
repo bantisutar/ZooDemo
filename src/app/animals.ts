0fe73303e5521427ef31f75b5b43ddbe8468f25b
Banti <anjneya@podiumsys.com>
2019-02-12 16:09:42 +0530

    add animal interface and change services with observable

---
+++ src/app/animals.ts
@@ -0,0 +1,10 @@
+export class Animals {
+	type: string;
+    category: string;
+    breed: string;
+    height: number;
+    weight: number;
+    food: string;
+    speed: number;
+    color: string;
+}
--- src/app/service/animal.service.ts
+++ src/app/service/animal.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs/Observable';
-//import { Animals } from '../_models';
+import { Observable, of, throwError } from 'rxjs';
+import { Animals } from '../animals';
+import { catchError, tap, map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 @Injectable({
   providedIn: 'root'
 })
@@ -10,28 +15,52 @@ export class AnimalService {
 config = environment.config;
   constructor(private http: HttpClient) { }
 
- getAll(){
-        return this.http.get(this.config.apiUrl+'/api/animals');
-    }
+getAll (): Observable<Animals[]> {
+    return this.http.get<Animals[]>(this.config.apiUrl+'/api/animals')
+      .pipe(
+        tap(animals => console.log('Fetch Animals')),
+        catchError(this.handleError('getProducts', []))
+      );
+  }
+
  getById(id: number) {
         return this.http.get(this.config.apiUrl+'/api/getbyId/' + id);
     }
 
 
-    register(complaint: any) {
-        //complaint.status = 1;
-        //complaint.mode = 'Save';
-        return this.http.post(this.config.apiUrl+'/api/SaveAnimals', complaint);
-    }
+ register (animal): Observable<Animals> {
+    return this.http.post<Animals>(this.config.apiUrl+'/api/SaveAnimals', animal, httpOptions).pipe(
+      tap((animal: Animals) => console.log(`added Animals w/ id=${animal._id}`)),
+      catchError(this.handleError<Animals>('register'))
+    );
+  }
 
-    registerNew(animal: any) {
-        animal.status = 1;
-        //complaint.mode = 'Save';
-        console.log("in service",animal);
-        return this.http.post(this.config.apiUrl+'/api/SavenewAnimals', animal);
-    }
-     deleteRecord(data: any) {
-        return this.http.post(this.config.apiUrl+'/api/deleteByid', data);
-    }
+  registerNew (animal): Observable<Animals> {
+      animal.status = 1;
+    return this.http.post<Animals>(this.config.apiUrl+'/api/SavenewAnimals', animal, httpOptions).pipe(
+      tap((animal: Animals) => console.log(`added Animals w/ id=${animal._id}`)),
+      catchError(this.handleError<Animals>('registerNew'))
+    );
+  }
+
+   deleteRecord (animal): Observable<Product> {
+
+      return this.http.post<Animals>(this.config.apiUrl+'/api/deleteByid', animal, httpOptions).pipe(
+      tap((animal: Animals) => console.log(`Deleted Animals w/ id=${animal._id}`)),
+      catchError(this.handleError<Animals>('deleteRecord'))
+    );
+  }
+
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
 
